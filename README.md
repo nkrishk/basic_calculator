0b7fc18e5160534159baf0045c6d1e0df6ad4fcd
Krishnakumar Natarajan (krinata2) <krinata2@cisco.com>
2024-05-29 20:26:39 +0530

    mend
    accept the port details as parameter and read me modification

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # basic_calculator
-gRPC service implementation for a basic calculator feature
+gRPC API service implementation for a basic calculator features
+
+## command to compile the .proto file
+
+
+```shell
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. calculator.proto
+```
+
+This will generate two files: calculator_pb2.py and calculator_pb2_grpc.py.
+
+## Run server
+
+```shell
+ python calculator_server.py --port=50051
+```
+or
+```shell
+ python3 calculator_server.py --port=50051
+ ```
+
+## Run client
+
+```shell
+python calculator_client.py
+```
+or
+```shell
+python3 calculator_client.py
+```
+
+## server port
+
+The server port is hardcoded to run
+```python3
+    server.add_insecure_port('[::]:50051')
+```
+
+## test using post man
+![img.png](img.png)
+
+![screen_shot/post_man.png](screen_shot/post_man.png)
--- calculator_server.py
+++ calculator_server.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import grpc
 import calculator_pb2
 import calculator_pb2_grpc
+import argparse
 
 class CalculatorServicer(calculator_pb2_grpc.CalculatorServicer):
     def Add(self, request, context):
@@ -25,9 +26,13 @@ class CalculatorServicer(calculator_pb2_grpc.CalculatorServicer):
         return calculator_pb2.Result(value=result)
 
 def serve():
+    parser = argparse.ArgumentParser(description='gRPC API for basic calculator')
+    # parser.add_argument('--port', required=True, type=int, action='store_true', help='gRPC server port number')
+    parser.add_argument('--port', required=True, type=int)
+    args = parser.parse_args()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     calculator_pb2_grpc.add_CalculatorServicer_to_server(CalculatorServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{args.port}')
     server.start()
     server.wait_for_termination()
 
